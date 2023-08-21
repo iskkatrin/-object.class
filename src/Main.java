9aafa6c8eab1925158922627e8824ce3609bfd20
Ekaterina <klarina2409@gmail.com>
2023-08-22 01:02:15 +0300

    HM2

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         Author author = new Author("Иван ","Тургенев");
-        Book book = new Book("Муму",author, 1854);
+        Book book = new Book("Муму", author, 1854);
         System.out.println("Книга: " + book.getPublisher());
-        System.out.println("Автор: " + book.getAuthor());
+        System.out.println("Автор: " + book.getAuthor().getName() + book.getAuthor().getSurName());
         System.out.println("Год издания: " + book.getPublicationYear());
 
         book.setPublicationYear(2000);
@@ -13,7 +13,7 @@ public class Main {
         Author author1 = new Author("Лев ", "Толстой");
         Book book1 = new Book("Анна Каренина ", author, 1877);
         System.out.println("Книга: " + book1.getPublisher());
-        System.out.println("Автор: " + book1.getAuthor());
+        System.out.println("Автор: " + book1.getAuthor().getName() + book1.getAuthor().getSurName());
         book1.setPublicationYear(1900);
         System.out.println("Год переиздания: " + book1.getPublicationYear());
 
